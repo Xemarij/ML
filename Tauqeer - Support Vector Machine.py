a5e907ca63b434af74d878330439a7867a6787a3
Xemarij <70664912+Xemarij@users.noreply.github.com>
2020-09-09 10:32:01 +0500

    Add files via upload

---
+++ Tauqeer - Support Vector Machine.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 08:57:46 2020
+
+@author: Xemarij
+
+Support Vector Machine 
+
+Created on Fri Sep  4 16:02:35 2020
+
+@author: Xemarij
+"""
+
+#importing the libraies
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, 4]
+
+
+
+#Encoding categorical data (The following code in comments has been depricated)
+
+""" 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+LabelEncoder_X = LabelEncoder()
+X[:,0] =  LabelEncoder_X.fit_transform(X[:,0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+"""
+
+#The above code should be like in the following
+"""
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+labelencoder_X = LabelEncoder()
+from sklearn.compose import ColumnTransformer
+
+ct = ColumnTransformer(
+    [('one_hot_encoder', OneHotEncoder(), [0])],    # The column index to be transformed (here is [0] but can be [0, 1, 3])
+    remainder='passthrough'                         # Leave the rest of the columns untouched
+)
+
+X = np.array(ct.fit_transform(X), dtype=np.float)
+
+#The output variable is LabelEncoded the following way
+labelencoder_y = LabelEncoder()
+y = labelencoder_X.fit_transform(y)
+
+#Splitting the data into training set and test set
+#The code in the coments is depricated. The code following it is valid. 
+"""
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+
+
+#Feature Scalling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+#Fitting SVM to the training set
+from sklearn.svm import SVC
+classifier = SVC(kernel='linear', random_state=0)
+classifier.fit(X_train, y_train)
+
+
+#Predicting the test set results
+y_pred = classifier.predict(X_test)
+ 
+#Making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+ 
+#Visualizing the training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1,X2 = np.meshgrid(np.arange(start = X_set[:, 0].min()-1, stop =X_set[:,0].max()+1,step=0.01),
+                    np.arange(start = X_set[:, 1].min()-1, stop =X_set[:,1].max()+1,step=0.01))
+
+# Building the graph contour based on classification method
+Z_train = np.array([X1.ravel(), X2.ravel()]).T
+
+plt.contourf(X1, X2, classifier.predict(Z_train).reshape(X1.shape), alpha=0.75,
+                                                         cmap=ListedColormap(
+                                                             ('red', 'green')))
+# Apply limits when outliers are present
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+# Creating the scatter plot of the Training set results
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j,
+                                                              1],
+                c=ListedColormap(('red', 'green'))(i), label=j)
+    
+plt.title('SVM (Trainning set results)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+#Visualizing the test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1,X2 = np.meshgrid(np.arange(start = X_set[:, 0].min()-1, stop =X_set[:,0].max()+1,step=0.01),
+                    np.arange(start = X_set[:, 1].min()-1, stop =X_set[:,1].max()+1,step=0.01))
+
+# Building the graph contour based on classification method
+Z_train = np.array([X1.ravel(), X2.ravel()]).T
+
+plt.contourf(X1, X2, classifier.predict(Z_train).reshape(X1.shape), alpha=0.75,
+                                                         cmap=ListedColormap(
+                                                             ('red', 'green')))
+# Apply limits when out liers are present
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+# Creating the scatter plot of the Training set results
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j,
+                                                              1],
+                c=ListedColormap(('red', 'green'))(i), label=j)
+    
+plt.title('SVM (Test set results)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
